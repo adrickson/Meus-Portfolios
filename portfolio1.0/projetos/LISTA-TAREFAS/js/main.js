a11e49803cc330a22e0925c548d255ed5c2c3615
adrickson <144737887+adrickson@users.noreply.github.com>
2024-06-13 19:51:30 -0300

    adicionando as tarefas no localStorage do navegador

--- portfolio1.0/projetos/LISTA-TAREFAS/js/main.js
+++ portfolio1.0/projetos/LISTA-TAREFAS/js/main.js
@@ -1,9 +1,9 @@
 const btnAdicionarTarefa = document.querySelector('#abreJanela');
 const contTarefas = document.querySelector('.cont_tarefas');
 
-const containerPaiDivs = document.querySelector('.lista-tarefas') // container pai onde armazenara os container que ficarão responsáveis por armazenar o conteúdo.
+const containerPaiDivs = document.querySelector('.lista-tarefas'); // container pai onde armazenara os container que ficarão responsáveis por armazenar o conteúdo.
 
-const paragrafoTemporario = document.querySelector('#avisoTemporario') // Aviso inserido até enviar o formulário
+const paragrafoTemporario = document.querySelector('#avisoTemporario'); // Aviso inserido até enviar o formulário
 
 // Variáveis do formulário de adicionar tarefa
 const formulario = document.querySelector('.formulario');
@@ -12,159 +12,217 @@ const btnCancelar = document.querySelector('#cancelar');
 const caixaInput = document.querySelector('#descricao');
 
 // Variáveis do formulário da Janela de Editar tarefa
-
 const btnCancelarTarefa = document.querySelector('#btnCancelarEdicao');
 const inputEditarTarefa = document.querySelector('#inputEditaTarefa');
 const btnFinalizarEdicao = document.querySelector('#btnFinalizarEdicao');
 const txtMostradoJanelaEditar = document.querySelector('#textoASerMostrado');
 
 let tarefaParaEditar = null;
+let idParaEditar = null;
 
-function abreJanela(){
-
+function abreJanela() {
     // Função para Abrir o PopUp
-
     contTarefas.style.display = 'none';
 
-    if(contTarefas.style.display == 'none') {
+    if (contTarefas.style.display == 'none') {
         contTarefas.style.display = 'block';
     }
+}
 
-    // Função para Fechar o PopUp
-
-    function fechaJanela(){
-        if(contTarefas.style.display == 'block'){
-            contTarefas.style.display = 'none';
-            caixaInput.value = '';
-        }
+// Função para Fechar o PopUp
+function fechaJanela() {
+    if (contTarefas.style.display == 'block') {
+        contTarefas.style.display = 'none';
+        caixaInput.value = '';
     }
+}
 
-    btnCancelar.addEventListener('click', fechaJanela);
+btnCancelar.addEventListener('click', fechaJanela);
 
-    // Função de criar o ícone de editar
+btnAdicionarTarefa.addEventListener('click', abreJanela);
 
-    function criaIconeEditar(){
+// FUNÇÃO DE APARECER JANELA DE EDITAR TAREFA
+function abreJanelaDeEditar(tarefa, id) {
+    // A janela de editar está recebendo o parágrafo da div em que o ícone de edição foi clicado.
+    const janelaDeEdicaoTarefa = document.querySelector('.janelaEditarTarefa');
+    inputEditarTarefa.value = '';
+    tarefaParaEditar = tarefa; // A tarefa que foi definida como null, está recebendo o texto da tarefa
+    tarefaAserEditada = tarefaParaEditar.innerHTML;
+    txtMostradoJanelaEditar.innerHTML = `" ${tarefaAserEditada} "`;
+    idParaEditar = id;
+    abrePopUp(janelaDeEdicaoTarefa);
+}
 
-        const iconeEditar = document.createElement('span');
-        iconeEditar.classList.add('material-symbols-outlined');
-        iconeEditar.innerHTML = 'draw';
+// Função de criar o ícone de editar
+function criaIconeEditar() {
+    const iconeEditar = document.createElement('span');
+    iconeEditar.classList.add('material-symbols-outlined');
+    iconeEditar.innerHTML = 'draw';
+    return iconeEditar;
+}
 
-        return iconeEditar;
-    }
+// Função de criar ícone de deletar
+function criaIconeDeletar() {
+    const iconeDeletar = document.createElement('span');
+    iconeDeletar.classList.add('material-symbols-outlined');
+    iconeDeletar.innerHTML = 'delete';
+    return iconeDeletar;
+}
 
-    // Função de criar ícone de deletar
+// Função de Adicionar tarefa
+function adicionaTarefa() {
+    const texto = caixaInput.value;
 
-    function ciraIconeDeletar(){
-        const iconeDeletar = document.createElement('span');
-        iconeDeletar.classList.add('material-symbols-outlined');
-        iconeDeletar.innerHTML = 'delete';
+    if (texto === '') {
+        paragrafoTemporario.style.display = 'block';
+    } else {
+        paragrafoTemporario.innerHTML = ``;
+        const criaP = document.createElement('p');
+        criaP.innerHTML = texto;
+        const criaDiv = document.createElement('div');
+        criaDiv.classList.add('armazena_texto');
 
-        return iconeDeletar;
-    }
+        const iconeEditar = criaIconeEditar();
+        const iconeDeletar = criaIconeDeletar();
+        const id = Date.now();
 
-    // Função de Adicionar tarefa
-
-    function adicionaTarefa(){
-        const texto = caixaInput.value;
-
-        if(texto === ''){
-            paragrafoTemporario.style.display = 'block';
-        } else {
-            paragrafoTemporario.innerHTML = `<p>Minhas Tarefas:</p>`;
-
-            const criaP = document.createElement('p');
-            criaP.innerHTML = texto;
-
-            const criaDiv = document.createElement('div');
-            criaDiv.classList.add('armazena_texto');
-
-            const iconeEditar = criaIconeEditar();
-            const iconeDeletar = ciraIconeDeletar();
-
-            iconeEditar.addEventListener('click', function (){
-                abreJanelaDeEditar(criaP); // chamando o evento de abrir a janela de editar e passando o paragrafo como parâmetro.
-            });
-
-            criaDiv.appendChild(criaP);
-            criaDiv.appendChild(iconeEditar);
-            criaDiv.appendChild(iconeDeletar);
-            
-            containerPaiDivs.appendChild(criaDiv);
-            
-            
-            fechaJanela();
-        }
-        
-    }
-
-    btnFinalizar.addEventListener('click', adicionaTarefa);
-
-    // CRIANDO FUNÇÃO EXCLUSIVA PARA EXIBIR JANELAS (DIVS, SECTIONS, ETC)
-
-    function abrePopUp(janelaASerAberta){
-        janelaASerAberta.style.display = 'none';
-
-        if (janelaASerAberta.style.display === 'none'){
-            janelaASerAberta.style.display = 'block';
-        }
-
-        return janelaASerAberta;
-    }
-
-    // CRIANDO FUNÇÃO EXCLUSIVA PARA FECHAR JANELAS (DIVS, SECTIONS, ETC)
-
-    function fechaPopUp(janelaASerFechada){
-
-        if(janelaASerFechada.style.display === 'block'){
-            janelaASerFechada.style.display = 'none';
-        }
-    }
-
-
-    // FUNÇÃO DE APARECER JANELA DE EDITAR TAREFA
-
-    function abreJanelaDeEditar(tarefa){ // A janela de editar está recebendo o parágrafo da div em que o ícone de edição foi clicado.
-
-        const janelaDeEdicaoTarefa = document.querySelector('.janelaEditarTarefa');
-        inputEditarTarefa.value = '';
-        tarefaParaEditar = tarefa; // A tarefa que foi definida como null, está recebendo o texto da tarefa
-        tarefaAserEditada = tarefaParaEditar.innerHTML;
-        txtMostradoJanelaEditar.innerHTML = `" ${tarefaAserEditada} "`;
-        abrePopUp(janelaDeEdicaoTarefa);
-    }
-
-        //FUNÇÃO DE FINALIZAR EDIÇÃO
-
-        btnFinalizarEdicao.addEventListener('click', function(){
-
-            if(tarefaParaEditar){ // Verificando se a tarefa tem o estado True.
-                tarefaParaEditar.innerHTML = inputEditarTarefa.value; // A tarefa da lista de tarefas está recebendo o valor do input da janela de editar terefa.
-                const janelaDeEdicaoTarefa = document.querySelector('.janelaEditarTarefa');
-                fechaPopUp(janelaDeEdicaoTarefa);
-                inputEditarTarefa.value = '';
-                tarefaParaEditar = null;
-            }
-
+        iconeEditar.addEventListener('click', function () {
+            abreJanelaDeEditar(criaP, id); // chamando o evento de abrir a janela de editar e passando o parágrafo como parâmetro.
         });
-    
-    // FUNÇÃO DE FECHAR A JANELA DE EDITAR TAREFA
 
-    btnCancelarTarefa.addEventListener('click', function(){
+        iconeDeletar.addEventListener('click', function () {
+            deletaTarefa(criaDiv, criaP, id); // chama a função de deletar tarefa passando a div em qual a tarefa está armazenada.
+        });
+
+        criaDiv.appendChild(criaP);
+        criaDiv.appendChild(iconeEditar);
+        criaDiv.appendChild(iconeDeletar);
+
+        containerPaiDivs.appendChild(criaDiv);
+
+        salvarTarefaLocalStorage(texto, id); // chamando a função de salvar tarefas no localStorage do navegador, passando como parâmetro o texto que estará presente no parágrafo.
+
+        fechaJanela();
+    }
+}
+
+btnFinalizar.addEventListener('click', adicionaTarefa);
+
+// CRIANDO FUNÇÃO EXCLUSIVA PARA EXIBIR JANELAS (DIVS, SECTIONS, ETC)
+function abrePopUp(janelaASerAberta) {
+    janelaASerAberta.style.display = 'none';
+
+    if (janelaASerAberta.style.display === 'none') {
+        janelaASerAberta.style.display = 'block';
+    }
+
+    return janelaASerAberta;
+}
+
+// CRIANDO FUNÇÃO EXCLUSIVA PARA FECHAR JANELAS (DIVS, SECTIONS, ETC)
+function fechaPopUp(janelaASerFechada) {
+    if (janelaASerFechada.style.display === 'block') {
+        janelaASerFechada.style.display = 'none';
+    }
+}
+
+// FUNÇÃO DE FINALIZAR EDIÇÃO
+btnFinalizarEdicao.addEventListener('click', function () {
+    if (tarefaParaEditar) { // Verificando se a tarefa tem o estado True.
+        const novoTexto = inputEditarTarefa.value;
+        tarefaParaEditar.innerHTML = novoTexto; // A tarefa da lista de tarefas está recebendo o valor do input da janela de editar tarefa.
+
+        editarTarefaLocalStorage(idParaEditar, novoTexto);
+
         const janelaDeEdicaoTarefa = document.querySelector('.janelaEditarTarefa');
+
         fechaPopUp(janelaDeEdicaoTarefa);
 
         inputEditarTarefa.value = '';
-    })
 
-    // Função para evitar o envio do formulário
-
-    function cancelaOperacao(evento) {
-        evento.preventDefault(); // Evita o envio do formulário.
-
+        tarefaParaEditar = null;
+        idParaEditar = null;
     }
+});
 
-    formulario.addEventListener('submit', cancelaOperacao);
+// FUNÇÃO DE FECHAR A JANELA DE EDITAR TAREFA
+btnCancelarTarefa.addEventListener('click', function () {
+    const janelaDeEdicaoTarefa = document.querySelector('.janelaEditarTarefa');
+    fechaPopUp(janelaDeEdicaoTarefa);
 
+    inputEditarTarefa.value = '';
+})
+
+// FUNÇÃO DE DELETAR TAREFA
+function deletaTarefa(divTarefa, paragrafo, id) {
+    divTarefa.remove();
+    removerTarefaLocalStorage(id);
 }
 
-btnAdicionarTarefa.addEventListener('click', abreJanela);
+// Função para evitar o envio do formulário
+function cancelaOperacao(evento) {
+    evento.preventDefault(); // Evita o envio do formulário.
+}
+
+formulario.addEventListener('submit', cancelaOperacao);
+
+// CRIANDO LÓGICA DE SALVAR TAREFAS NO BANCO DE DADOS DO NAVEGADOR (LOCALSTORAGE)
+function salvarTarefaLocalStorage(tarefa, id) {
+    let tarefas = localStorage.getItem('tarefas') ? JSON.parse(localStorage.getItem('tarefas')) : []; // se localStorage.getItem('tarefas') existir ele é convertido para um array, se não existir é convertido para um array vazio e armazenado na variável tarefas.
+
+    tarefas.push({ id, texto: tarefa }); // estou enviando um Objeto com o texto da tarefa e seu identificador.
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas)); // estou setando no localStorage um JSON de string.
+}
+
+function carregarTarefas() { // Essa função é inicializada assim que a página é carregada
+    let tarefas = localStorage.getItem('tarefas') ? JSON.parse(localStorage.getItem('tarefas')) : []; // se localStorage.getItem('tarefas') existir ele é convertido para um array, se não existir é convertido para um array vazio e armazenado na variável tarefas.
+
+    tarefas.forEach(function ({ id, texto }) { // o foreach itera sobre cada item do localStorage e em seguida sua função adiciona os elementos ao dom.
+        const criaP = document.createElement('p');
+        criaP.innerHTML = texto;
+
+        const criaDiv = document.createElement('div');
+        criaDiv.classList.add('armazena_texto');
+
+        const iconeEditar = criaIconeEditar();
+        iconeEditar.addEventListener('click', function () {
+            abreJanelaDeEditar(criaP, id);
+        });
+
+        const iconeDeletar = criaIconeDeletar();
+        iconeDeletar.addEventListener('click', function () {
+            deletaTarefa(criaDiv, criaP, id);
+        });
+
+        criaDiv.appendChild(criaP);
+        criaDiv.appendChild(iconeEditar);
+        criaDiv.appendChild(iconeDeletar);
+
+        containerPaiDivs.appendChild(criaDiv);
+    });
+}
+
+function editarTarefaLocalStorage(id, novoTexto) {
+    let tarefas = localStorage.getItem('tarefas') ? JSON.parse(localStorage.getItem('tarefas')) : [];
+
+    const index = tarefas.findIndex(tarefa => tarefa.id === id); // encontrar a tarefa pelo id
+    if (index !== -1) { // verifica se encontrou a posição da tarefa antiga, se não encontrou ele recebe -1 e o array de tarefas recebe a nova tarefa na posição da antiga.
+        tarefas[index].texto = novoTexto;
+    }
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function removerTarefaLocalStorage(id) {
+    let tarefas = localStorage.getItem('tarefas') ? JSON.parse(localStorage.getItem('tarefas')) : [];
+
+    const index = tarefas.findIndex(tarefa => tarefa.id === id);
+    if (index !== -1) {
+        tarefas.splice(index, 1);
+    }
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+carregarTarefas();
